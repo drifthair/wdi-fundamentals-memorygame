fa257a8c4efc0198bc23ba65d2da384a1a94107f
CeCe Nie <cece_nie@glic.com>
2018-09-25 21:32:41 -0400

    created game logic

--- memeory_game/js/main.js
+++ memeory_game/js/main.js
@@ -1,18 +1,22 @@
 var cards = ["queen", "queen", "king", "king"];
 var cardsInPlay = [];
-var cardOne = cards[0];
-var cardTwo = cards[2];
 
-cardsInPlay.push(cardOne);
-cardsInPlay.push(cardTwo);
-
-console.log("user flipped " + cardsInPlay);
-
-if (cardsInPlay.length === 2) {
+var checkForMatch = function() {
     if ( cardsInPlay[0] === cardsInPlay[1] ) {
         alert("You found a match!");
     }
     else {
         alert("Sorry, try a again.");
     }
-}
+};
+
+var flipCard = function(cardId) {
+    console.log("User flipped " + cards[cardId]);
+    cardsInPlay.push(cards[cardId]);
+    checkForMatch();
+};
+
+flipCard(0);
+flipCard(2); 
+
+
