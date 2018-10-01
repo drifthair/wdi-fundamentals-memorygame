86bab949de17243ed7ce3a408cf67ce2a061da16
CeCe Nie <cece_nie@glic.com>
2018-10-01 17:48:29 -0400

    created game logic

--- memeory_game/js/main.js
+++ memeory_game/js/main.js
@@ -23,24 +23,50 @@ var cards = [
 
 var cardsInPlay = [];
 
-var checkForMatch = function() {
-    if ( cardsInPlay[0] === cardsInPlay[1] ) {
-        alert("You found a match!");
-    }
-    else {
-        alert("Sorry, try a again.");
+var score = 0;
+
+var checkForMatch = function(id) {
+    if ( cardsInPlay.length > 1) {
+        if ( cardsInPlay[0] === cardsInPlay[1] ) {
+            score +=1;
+            alert("You found a match! Your scrore is " + score);
+            
+        }
+        else {
+            alert("Sorry, try a again.");
+        }
     }
 };
 
-var flipCard = function(cardId) {
-    console.log("User flipped " + cards[cardId].rank);
-    console.log(cards[cardId].cardImage);
-    console.log(cards[cardId].suit);
-    cardsInPlay.push(cards[cardId].rank);
-    checkForMatch();
+var flipCard = function() {
+     var cardId = this.getAttribute('data-id');
+     console.log("User flipped " + cards[cardId].rank);
+     console.log(cards[cardId].cardImage);
+     this.setAttribute('src', cards[cardId].cardImage);
+     console.log(cards[cardId].suit);
+     cardsInPlay.push(cards[cardId].rank);
+     checkForMatch(cardId);
+ };
+
+var createBoard = function() {
+    for ( var i = 0; i < cards.length; i++ ) {
+        var cardElement = document.createElement('img');
+        cardElement.setAttribute('src', '../images/back.png');
+        cardElement.setAttribute('data-id', i);
+        cardElement.addEventListener('click', flipCard);
+        document.getElementById('game-board').appendChild(cardElement);
+    }
 };
 
-flipCard(0);
-flipCard(2); 
+var resetBoard = function() {
+    for ( var j = 0; j < document.getElementById('game-board').children.length; j++ ) {
+        var ele = document.querySelectorAll('img');
+        ele[j].setAttribute('src', '../images/back.png');
+
+        cardsInPlay.length = 0;
+    }
+}
+
+createBoard();
 
 
